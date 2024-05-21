aaf46d1bfa97f68fb27b62517c21453075355e8e
gabriel19ara <gabriel19ara@gmail.com>
2024-05-21 19:44:10 -0300

    incluindo o mock de produtos

---
+++ api.js
@@ -0,0 +1,11 @@
+const produtos = [
+{
+    nome: "tenis",
+    preco: 1000,
+    desconto: 0.15,
+    descricao: "branco",
+    image: '.assets/images/mask grouptenis.png'
+}
+]
+
+export { produtos  }
---
+++ main.js
@@ -0,0 +1,5 @@
+import { produtos } from './api.js'
+
+let { nome, preco} = produtos[produtos.length - 1]; 
+
+console.log(nome, preco);
